2bdbb35546cf41c9def82f4fcdd223adc5ae9a0b
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-05-21 16:19:29 -0400

    basic list returned in React. need to add HTML to List.js

--- src/composition/App.js
+++ src/composition/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './List';
 
-function App() {
+function App(props) {
+  const participants = props.store;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-instance">
+      <div className="participant-list">
+        {participants.map(participant => (
+          <List key={participant.id} name={participant.name} avatar={participant.avatar} inSession={participant.inSession} />)
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/composition/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function List(props) {
+  
+  console.log(props)
+  return (
+
+  <div className={props.name}> </div>
+  )  
+}
+
+export default List;
