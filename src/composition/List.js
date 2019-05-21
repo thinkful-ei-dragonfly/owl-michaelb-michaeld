7b0b77a4f7372714b7af4ed36799593a76071de5
Michael Bravo <michael.bravo777@gmail.com>
2019-05-21 16:58:12 -0400

    added participant list

--- src/composition/List.js
+++ src/composition/List.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './List.css';
 
 function List(props) {
-  
-  console.log(props)
   return (
+    <div className="session-participant">
+      <img src={props.avatar} alt={props.name} width="40px"/>
+      {props.name}
+      {props.inSession ? (
+        <div className="inSession" />
+      ) : (
+        <div className="notInSession" />
+      )}
 
-  <div className={props.name}> </div>
+    </div>
   )  
 }
 
